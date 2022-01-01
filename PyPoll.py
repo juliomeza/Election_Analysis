dd23044e3e5a43ce9ed70adb28f7a40b51d5295e
Julio Meza <juliomeza@gmail.com>
2022-01-01 08:13:47 -0500

    Adding PyPoll File

--- PyPoll.py
+++ PyPoll.py
@@ -1,4 +1,17 @@
+import csv
+
+
 # The data we need to retrieve.
+# Path and File Name
+file_to_load = 'Resources/election_results.csv'
+
+# Open the file in read mode
+with open(file_to_load) as election_data:
+    print(election_data)
+
+
+
+
 
 # 1. The total number of votes
 # 2. A complete list of candidates who received votes
