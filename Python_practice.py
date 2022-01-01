6dc8e6972952c2577d722307ec08b789bceebde3
Julio Meza <juliomeza@gmail.com>
2021-12-31 21:24:41 -0500

    Python Files

--- Python_practice.py
+++ Python_practice.py
@@ -1,6 +1,13 @@
-temperature = int(input("What is the temperature outside? "))
+counties = ["Arapahoe","Denver","Jefferson"]
 
-if temperature > 80:
-    print("Turn on the AC.")
+if "El Paso" in counties:
+    print("El Paso is in the list of counties")
 else:
-    print("Open the windows.")
+    print("El Paso is not in the list of counties")
+
+
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso are in the list of counties.")
+else:
+    print("Arapahoe and El Paso are not in the list of counties.")
+
